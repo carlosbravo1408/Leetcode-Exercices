37347af4c161f8b5591b4c03ef4eb8aa8a61c29e
carlos <carlos.j.bravo@hotmail.com>
2024-11-25 11:43:33 -0500

    refactor: reverse bit unsigned int 32 bits code added

--- reverse_bits.py
+++ reverse_bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        n = n & 0xFFFFFFFF
+        # n = n & 0xFFFFFFFF
         n = ((n >> 16) | (n << 16)) & 0xFFFFFFFF
         n = (((n & 0xFF00FF00) >> 8) | ((n & 0x00FF00FF) << 8)) & 0xFFFFFFFF
         n = (((n & 0xF0F0F0F0) >> 4) | ((n & 0x0F0F0F0F) << 4)) & 0xFFFFFFFF
