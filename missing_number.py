72b32beeb7b065f5bfb91203409025d597cb6833
carlos <carlos.j.bravo@hotmail.com>
2024-11-26 20:02:36 -0500

    refactor: added easy exercises

---
+++ missing_number.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        data = set(nums)
+        for i in range(len(nums)+1):
+            if i not in data:
+                return i
