9f05de41e63f941bf810ae4eb521f6f7cbd9ae0a
carlos <carlos.j.bravo@hotmail.com>
2024-11-27 02:00:41 -0500

    refactor: added valid-parentheses

---
+++ 0020_valid_parentheses.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/valid-parentheses/description/
+from queue import Queue
+
+
+class Solution:
+    OPEN = ["(", "[", "{"]
+    def isValid(self, s: str) -> bool:
+        data = list()
+        for symbol in s:
+            if symbol in self.OPEN:
+                data.append(symbol)
+            elif len(data) <= 0:
+                return False
+            elif symbol == ")":
+                if data[-1] == '(':
+                    data.pop()
+                else:
+                    return False
+            elif symbol == "]":
+                if data[-1] == '[':
+                    data.pop()
+                else:
+                    return False
+            elif symbol == "}":
+                if data[-1] == '{':
+                    data.pop()
+                else:
+                    return False
+        return len(data) == 0
