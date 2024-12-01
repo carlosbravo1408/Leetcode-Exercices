fadbe5dd2b53db05c3abd28430e5228501ac113f
carlos <carlos.j.bravo@hotmail.com>
2024-12-01 00:45:00 -0500

    refactor: modified magic number in mySqrt

--- 0069_sqrt.py
+++ 0069_sqrt.py
@@ -9,11 +9,24 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         bytes_values = struct.pack("!f", x)
         i = int.from_bytes(bytes_values, 'big', signed=False)
-        i = 0x1FBD1DF5 + (i >> 1)
+        i = 0x1FBD1DF5 + (i >> 1)  # con sigma 0.04504656791687012
         y = int(i).to_bytes(4, byteorder="big", signed=False)
         y = struct.unpack("!f", y)[0]
         y = y * 0.5 * (1 + x / (y * y))
         y = y * 0.5 * (1 + x / (y * y))
+        y = y * 0.5 * (1 + x / (y * y))
+        y = y * 0.5 * (1 + x / (y * y))
+        y = y * 0.5 * (1 + x / (y * y))
+        return int(y)
+
+    # requiere al menos 3 iteraciones para cumplir todos los casos, con un
+    # tiempo de 0ms de ejecucion
+    def mySqrt2(self, x: int) -> int:
+        bytes_values = struct.pack("!f", x)
+        i = int.from_bytes(bytes_values, 'big', signed=False)
+        i = 0x1FBD2B25 + (i >> 1)  # con sigma igual a 0.044241507848113315
+        y = int(i).to_bytes(4, byteorder="big", signed=False)
+        y = struct.unpack("!f", y)[0]
         y = y * 0.5 * (1 + x / (y * y))
         y = y * 0.5 * (1 + x / (y * y))
         y = y * 0.5 * (1 + x / (y * y))
@@ -34,7 +47,7 @@ class Solution:
         threehalfs = 1.5
         bytes_values = struct.pack("!f", x)
         i = int.from_bytes(bytes_values, byteorder="big", signed=False)
-        i = 0x5f3759df - (i >> 1)
+        i = 0x5f3759df - (i >> 1)  # con sigma 0.04504656791687012
         y = int(i).to_bytes(4, byteorder="big", signed=False)
         y = struct.unpack("!f", y)[0]
         for _ in range(iterations):
@@ -47,7 +60,7 @@ class Solution:
         threehalfs = 1.5
         bytes_values = struct.pack("!f", x)
         i = int.from_bytes(bytes_values, byteorder="big", signed=False)
-        i = 0x5f378171 - (i >> 1)
+        i = 0x5f378171 - (i >> 1)  # con sigma igual a 0.044241507848113315
         y = int(i).to_bytes(4, byteorder="big", signed=False)
         y = struct.unpack("!f", y)[0]
         for _ in range(iterations):
